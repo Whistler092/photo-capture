0b5ef2548f3599c57820015c670e05d5ea7e3bd4
Whistler092 <iamrmairoo@gmail.com>
2018-11-13 23:41:48 -0500

    Updates

---
+++ precache-manifest.c595d36cbb57ca5c1f1516f78b359529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/photo-capture/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bbef5e50ee27442d49db",
+    "url": "/photo-capture/static/js/runtime~main.bbef5e50.js"
+  },
+  {
+    "revision": "0b6a7725456cc842cc87",
+    "url": "/photo-capture/static/js/main.0b6a7725.chunk.js"
+  },
+  {
+    "revision": "6ac04053389788905ccb",
+    "url": "/photo-capture/static/js/1.6ac04053.chunk.js"
+  },
+  {
+    "revision": "0b6a7725456cc842cc87",
+    "url": "/photo-capture/static/css/main.ecb4c046.chunk.css"
+  },
+  {
+    "revision": "34db1c5f62c3f6a2ae78f766efd54629",
+    "url": "/photo-capture/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/photo-capture/precache-manifest.c595d36cbb57ca5c1f1516f78b359529.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/photo-capture/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
